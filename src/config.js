21338cd44b37eb0b9d3ca9f2836f59ef580da63d
Syston Tigers Automation <automation@systontigers.com>
2025-09-24 03:40:11 +0100

    🔍 TRIGGER: Debug authentication file creation

--- src/config.js
+++ src/config.js
@@ -1948,3 +1948,4 @@ function resetConfig() {
 // JSON fix test - Wed, Sep 24, 2025  2:42:58 AM
 // YAML syntax fix test - Wed, Sep 24, 2025  3:28:00 AM
 // Debug workflow test - Wed, Sep 24, 2025  3:32:37 AM
+// DEBUG authentication test - Wed, Sep 24, 2025  3:40:11 AM
